3147fc4a1cacc62a15db20ee9caae6ee0ad5a984
Harmanpreet Singh <harmanharjeet2003@gmail.com>
2025-06-30 21:31:36 +0530

    Initial commit

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,36 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Home = () => {
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex items-center justify-center relative"
+    >
+      <RevealOnScroll>
+        <div className="text-center z-10 px-4">
+          
+          <h1 className="text-10xl md:text-7xl font-bold bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
+            Harmanpreet Singh
+          </h1>
+          <h2 className="text-2xl md:text-4xl font-bold inline-block px-4 py-2 text-slate-400">MERN STACK DEVELOPER</h2>
+          <div className="mt-10 flex justify-center space-x-4">
+            <a
+              href="#projects"
+              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+            >
+              View Projects
+            </a>
+
+            <a
+              href="#contact"
+              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
+             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
+            >
+              Contact Me
+            </a>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,177 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">Spotify Clone</h3>
+              <p className="text-gray-400 mb-4">
+                ➞ Frontend Functionality: Built an interactive music player interface with play, pause, seek, and track navigation features, closely mimicking Spotify’s UI/UX.
+              </p>
+              <p className="text-gray-400 mb-4">
+                ➞ Full-Stack Expansion: Currently extending the project into a full-stack application with backend integration, user playlists, and real-time music streaming capabilities.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React","Tailwind CSS","JavaScript"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/Harmanpreet-11/spotify-clone-react"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">BLog App</h3>
+              <p className="text-gray-400 mb-4">
+                ➞ A Blogging App to write and share you ideas on different field's, letting more like minded people to connnect and share their thought's.
+              </p>
+              <p className="text-gray-400 mb-4">
+                ➞ Full-Stack Expansion: Currently extending the project into a full-stack application with Admin Panel and user interaction functionalities to edit and update their blogs. 
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Next JS", "Tailwind CSS","Mongo DB"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/Harmanpreet-11/Bloggin-App"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Youtube Autopause - Extension</h3>
+              <p className="text-gray-400 mb-4">
+                A powerfull chrome Extension for autoplaying youtube video's, build using Javascript , where you can switch tabs while watching Youtube without manually pausing and replaying the video.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Javascript"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/Harmanpreet-11/yt-autopause"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Portfolio Website</h3>
+              <p className="text-gray-400 mb-4">
+                My Personal Portfolio Website built to showcase all my Skill's and Interests.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Vite", "JavaScript","TailwindCSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="https://github.com/Harmanpreet-11/personal-portfolio"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
